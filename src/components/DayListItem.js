ce4f7edc8295dc74c9de973c104184ac8e3eedec
Michael Law <mikelaw86@hotmail.com>
2022-04-19 09:28:32 -0400

    Configure styling for daylistitem.js

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,33 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
+  const spotsAvailable = (spots) => {
+    if (spots === 0) {
+      return "no spots available";
+    } else if (spots === 1) {
+      return `${spots} spot available`;
+    } else {
+      return `${spots} spots available`;
+    }
+  };
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li
+      data-testid="day"
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <>
+        <h3 className="text--light">{spotsAvailable(props.spots)}</h3>
+      </>
     </li>
   );
 }
