cb866a8de9c11643c2fd92cd6130990b6073f008
Michael Law <mikelaw86@hotmail.com>
2022-04-19 11:23:36 -0400

    Create and insert nav container for days

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,5 +2,16 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  return <ul></ul>;
+  const days = props.days.map((day) => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        select={day.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+  return <ul>{days}</ul>;
 }
