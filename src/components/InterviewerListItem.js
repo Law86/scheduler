cc70d122c431887af00b07acb9be6bc11a4b2f38
Michael Law <mikelaw86@hotmail.com>
2022-04-20 11:07:53 -0400

    Add event listener to interviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,6 +3,10 @@ import classNames from "classnames";
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
   return (
     <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
