2edc38b047a676717d0c4b58eeb0c34b5237eda2
Michael Law <mikelaw86@hotmail.com>
2022-04-22 18:22:16 -0400

    Start implementation of useVisualMode for edit and create

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,34 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = 'CREATE';
+
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {!props.interview && <Empty />}
-      {props.interview && <Show
-        student={props.interview.student}
-        interviewer={props.interview.interviewer}
-      />}
+      {mode === EMPTY && (
+        <Empty
+          onAdd={() => {
+            return transition(CREATE);
+          }}
+        />
+      )}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />)}
     </article>
   )
 }
