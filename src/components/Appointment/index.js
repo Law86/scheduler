013732e644b60a20f4c700adb7e88e84d4d12025
Michael Law <mikelaw86@hotmail.com>
2022-04-22 16:26:35 -0400

    Refactor index.js in Appointment to accept && statements more clearly

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,28 +5,14 @@ import Show from "./Show";
 import Empty from "./Empty";
 
 export default function Appointment(props) {
-  function newAppt(props) {
-    return !props.time ? `No Appointments` : `Appointment at ${props.time}`;
-  }
-
-  if (!props.interview) {
-    return (
-      <article className="appointment">
-        {props.time}
-        <Empty />
-      </article>
-    );
-  } else if (props.interview) {
-    return (
-      <article className="appointment">
-        {props.time}
-        <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
-        />
-      </article>
-    );
-  }
-
-  return <article className="appointment">{newAppt(props)}</article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {!props.interview && <Empty />}
+      {props.interview && <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />}
+    </article>
+  )
 }
