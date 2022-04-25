bc8557ce63e064adae68e7bc6ba9bb07fd16066a
Michael Law <mikelaw86@hotmail.com>
2022-04-25 11:25:47 -0400

    Add further logic to bookinteriew and setting the state

--- src/components/Application.js
+++ src/components/Application.js
@@ -31,7 +31,18 @@ export default function Application(props) {
   }, []);
 
   function bookInterview(id, interview) {
-    console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    setState({
+      ...state,
+      appointments
+    });
   }
 
   const setDay = (day) => setState({ ...state, day });
