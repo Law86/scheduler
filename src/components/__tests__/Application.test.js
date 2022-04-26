e50dadafc889530d638478103ccf3db6c17e125b
Michael Law <mikelaw86@hotmail.com>
2022-04-26 11:06:26 -0400

    Add logic for first test for appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,9 +1,6 @@
 import React from "react";
-
 import { render, cleanup } from "@testing-library/react";
-
 import Application from "components/Application";
-
 afterEach(cleanup);
 
 it("renders without crashing", () => {
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Application from "components/Application";
-
+import Appointment from "components/Appointment"
 /*
   A test that renders a React Component
 */
 it("renders without crashing", () => {
   render(<Application />);
+});
+
+describe('Testing Appointment Component', () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  })
 });
